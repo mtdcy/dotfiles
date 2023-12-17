74b16641e8699d8c7768e81f96439d0c937a3d39
Chen Fang <mtdcy.chen@gmail.com>
2023-12-17 23:30:17 +0800

    zsh/history assume unchanged

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 
 git submodule update --init --recursive
 git submodule update --recursive --remote
+git update-index --assume-unchanged zsh/history 
 
 for i in bin bashrc zsh zshrc; do
     ln -svfT $PWD/$i $HOME/.$i 
