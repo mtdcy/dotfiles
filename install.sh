0fd4d2266b9dc25a097cc24ed142324c414e3fbb
mtdcy <mtdcy.chen@gmail.com>
2023-12-18 21:56:08 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,29 @@
 #!/bin/bash 
 #
 
-git submodule update --init --recursive
-git submodule update --recursive --remote
-git update-index --assume-unchanged zsh/history 
+pkg='brew'
+which apt && pkg='apt'
 
+which zsh || $pkg install zsh
+which vim || $pkg install vim
+
+chsh -s "$(which zsh)"
+
+EDITOR="$(which vim)"
+which nvim && EDITOR="$(which nvim)"
+
+if which update-alternatives; then
+    sudo update-alternatives --install "$(which editor)" editor "$EDITOR"
+    sudo update-alternatives --set editor "$EDITOR"
+fi
+
+git update-index --assume-unchanged zsh/history 
 for i in bin bashrc zsh zshrc; do
     ln -svfT $PWD/$i $HOME/.$i 
 done
 
-[ "$1" = "force" ] && cd nvim && ./install.sh 
+if [ "$1" = "all" ]; then
+    git submodule update --init --recursive
+    git submodule update --recursive --remote
+    cd nvim && ./install.sh 
+fi
