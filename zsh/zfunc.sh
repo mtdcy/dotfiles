d3a6e322660047a64ec8bc10289067e565c8dc24
Chen <mtdcy.chen@gmail.com>
2026-03-12 14:33:11 +0800

    update zsh completions

--- zsh/zfunc.sh
+++ zsh/zfunc.sh
@@ -2,20 +2,22 @@
 
 cd "$(dirname "$0")" || exit 1
 
-instdir="$(pwd -P)"
+INSTDIR="$(pwd -P)"
 
-workdir="$(mktemp -d)"
-trap "rm -rf $workdir" EXIT
+WORKDIR="$(mktemp -d)"
+trap "rm -rf $WORKDIR" EXIT
 
-cd "$workdir" || exit 1
+cd "$WORKDIR" || exit 1
 
-git clone --depth 1 https://github.com/zsh-users/zsh-completions.git 
+git clone --depth 1 https://github.com/zsh-users/zsh-completions.git
 
-mkdir -pv "$instdir/zsh-completions"
-cp -fv zsh-completions/src/_* "$instdir/zsh-completions/"
+mkdir -pv "$INSTDIR/zsh-completions"
+cp -fv zsh-completions/src/_* "$INSTDIR/zsh-completions/"
 
 # docker completion
-curl -sL -o "$instdir/zsh-completions/_docker" https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker
+curl -sL -o "$INSTDIR/zsh-completions/_docker" https://raw.githubusercontent.com/docker/cli/master/contrib/completion/zsh/_docker
 
 # shell integration
-curl -sL -o "$instdir/zsh-iterm2-shell-integration.zsh" https://iterm2.com/shell_integration/zsh
+curl -sL -o "$INSTDIR/zsh-iterm2-shell-integration.zsh" https://iterm2.com/shell_integration/zsh
+
+which openclaw && openclaw completion -s zsh > "$INSTDIR/zsh-completions/_openclaw"
